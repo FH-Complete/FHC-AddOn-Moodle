e93add2f7a7583d7956294f11ea4a730e33246e1
OliiverHacker <hacker@technikum-wien.at>
2020-10-22 13:40:44 +0200

    cleanup spaces with tabs

--- cis/course.php
+++ cis/course.php
@@ -8,37 +8,43 @@ require_once('../../../include/lehreinheit.class.php');
 require_once('../config.inc.php');
 require_once('../include/moodle_course.class.php');
 
-require_once(dirname(__FILE__).'/../config.inc.php');
-require_once(dirname(__FILE__).'/../include/moodle_course.class.php');
+require_once(dirname(__FILE__) . '/../config.inc.php');
+require_once(dirname(__FILE__) . '/../include/moodle_course.class.php');
 
 if (!$db = new basis_db())
-    die('Fehler beim Herstellen der Datenbankverbindung');
+{
+	die('Fehler beim Herstellen der Datenbankverbindung');
+}
 
 $user = get_uid();
 
-if(isset($_GET['stsem']))
-    $stsem = $_GET['stsem'];
-if(isset($_GET['courses']))
-    $courses = $_GET['courses'];
+if (isset($_GET['stsem']))
+{
+	$stsem = $_GET['stsem'];
+}
+if (isset($_GET['courses']))
+{
+	$courses = $_GET['courses'];
+}
 
 $moodle_courses = array();
 
 foreach ($courses as $course)
 {
-    $moodle_course = new moodle_course();
-    $lvId = $course['lvId'];
-    $leId = $course['leId'];
+	$moodle_course = new moodle_course();
+	$lvId = $course['lvId'];
+	$leId = $course['leId'];
 
-    if($moodle_course->course_exists_for_le($leId))
-    {
-        $moodle_course->getCourseByLeId($leId);
-    }
-    elseif($moodle_course->course_exists_for_lv($lvId,$stsem))
-    {
-        $moodle_course->getAll($lvId, $stsem);
-    }
+	if ($moodle_course->course_exists_for_le($leId))
+	{
+		$moodle_course->getCourseByLeId($leId);
+	}
+	elseif ($moodle_course->course_exists_for_lv($lvId, $stsem))
+	{
+		$moodle_course->getAll($lvId, $stsem);
+	}
 
-    array_push($moodle_courses, $moodle_course->result);
+	array_push($moodle_courses, $moodle_course->result);
 
 }
 
--- cis/init.js.php
+++ cis/init.js.php
@@ -24,71 +24,74 @@ require_once('../../../config/cis.config.inc.php');
 require_once('../config.inc.php');
 
 ?>
-if(typeof addon =='undefined')
-	var addon=Array();
+if (typeof addon == 'undefined')
+{
+	var addon = Array();
+}
 
 addon.push(
-{
-	init: function(page, params)
 	{
-		// Diese Funktion wird nach dem Laden der Seite im CIS aufgerufen
-
-		switch(page)
+		init: function (page, params)
 		{
-			case 'cis/private/lehre/benotungstool/lvgesamtnoteverwalten.php':
-				break;
+			// Diese Funktion wird nach dem Laden der Seite im CIS aufgerufen
 
-			case 'cis/private/lehre/lesson.php':
-				break;
-            case 'cis/private/lvplan/stpl_detail.php':
-                let courses = params.courses;
-                let stsem = params.stsem;
+			switch (page)
+			{
+				case 'cis/private/lehre/benotungstool/lvgesamtnoteverwalten.php':
+					break;
 
-                getCourseId(courses, stsem);
+				case 'cis/private/lehre/lesson.php':
+					break;
+				case 'cis/private/lvplan/stpl_detail.php':
+					let courses = params.courses;
+					let stsem = params.stsem;
 
-                break;
+					getCourseId(courses, stsem);
 
-            default:
-				break;
+					break;
+
+				default:
+					break;
+			}
 		}
-	}
-});
+	});
 
 function getCourseId(courses, stsem)
 {
-    $.ajax({
-        type: "GET",
-        url: '<?php echo APP_ROOT;?>addons/moodle/cis/course.php',
-        dataType: 'json',
-        data: {courses: courses, stsem: stsem},
-        success: function (result)
-        {
-            let moodle_courses = result.map(x => x[0]);
-            //checks if every element of moodle_courses is empty
-            //-> if yes then there are no Moodle Courses for any of the requested JSON params
-            if(!moodle_courses.every(x => isEmpty(x)))
-            {
-                let headerstag = '#stdplantablerow'
-                $(headerstag).append('<th>Moodle</th>')
+	$.ajax({
+		type: "GET",
+		url: '<?php echo APP_ROOT;?>addons/moodle/cis/course.php',
+		dataType: 'json',
+		data: {courses: courses, stsem: stsem},
+		success: function (result)
+		{
+			let moodle_courses = result.map(x => x[0]);
+			//checks if every element of moodle_courses is empty
+			//-> if yes then there are no Moodle Courses for any of the requested JSON params
+			if (!moodle_courses.every(x => isEmpty(x)))
+			{
+				let headerstag = '#stdplantablerow'
+				$(headerstag).append('<th>Moodle</th>')
 
-                for (i in moodle_courses)
-                {
-                        let link = '<?php echo ADDON_MOODLE_PATH;?>' + '/course/view.php?id=' + moodle_courses[i].mdl_course_id
-                    let tag = '#moodlelink' + (i)
+				for (i in moodle_courses)
+				{
+					let link = '<?php echo ADDON_MOODLE_PATH;?>' + '/course/view.php?id=' + moodle_courses[i].mdl_course_id
+					let tag = '#moodlelink' + (i)
 
-                    $(tag).append('<a href=' + link +  ' target="_blank">zum Moodlekurs</a>');
-                }
-            }
+					$(tag).append('<a href=' + link + ' target="_blank">zum Moodlekurs</a>');
+				}
+			}
 
-        },
-        error: function(){
-            console.log("ERROR WHILE MAKING AJAX CALL");
-        }
-    });
+		},
+		error: function ()
+		{
+			console.log("ERROR WHILE MAKING AJAX CALL");
+		}
+	});
 }
 
 function isEmpty(str)
 {
-    return (!str || 0 === str.length);
+	return (!str || 0 === str.length);
 }
 
