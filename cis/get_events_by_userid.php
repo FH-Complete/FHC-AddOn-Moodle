f45f97828c95ead695ab46a40445a373d83750f3
SimonGschnell <simongschnell@gmail.com>
2025-02-26 15:12:42 +0100

    feature(addon Moodle getUserCalenderEvents): adds a endpoint to the moodle addon to retrieve the calendar events of a moodle user

--- cis/get_events_by_userid.php
+++ cis/get_events_by_userid.php
@@ -2,6 +2,7 @@
 
 require_once('../lib/MoodleAPI.php');
 require_once('../config/config.php');
+require_once(dirname(__DIR__).'/../../config/cis.config.inc.php');
 
 
 $username = $_GET['username'];
@@ -13,6 +14,17 @@ header('Content-Type: application/json');
 //echo json_encode($result);
 // Call the method
 $events = $moodleAPI->fhcomplete_events_by_userid($username,$timestart,$timeend);
+$tz = new DateTimeZone(date_default_timezone_get());
+foreach($events->events as $event){
+	$timestart = new DateTime("@".$event->timestart);
+	$timestart->setTimezone($tz);
+	$event->timestart = $timestart->format(DateTime::ATOM);
+
+	$timeend = clone $timestart;
+	$timeend->modify('+'.$event->timeduration.' seconds');
+	$event->timeend = $timeend->format(DateTime::ATOM);
+}
+
 echo json_encode($events); 
 
 ?>
