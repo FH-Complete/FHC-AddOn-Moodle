7aa84eabd4653207e208bab6001d003d9e298adf
SimonGschnell <simongschnell@gmail.com>
2025-05-05 14:39:59 +0200

    update(Moodle Calendar Event): changes how the data is returned

--- Events.php
+++ Events.php
@@ -26,3 +26,17 @@ Events::on('lvMenuBuild', function ($menu_reference,$params) {
 	require_once(dirname(__FILE__).'/cis/menu_lv.inc.php');
 });
 
+
+Events::on('moodleCalendarEvents', function ($moodle_events_reference, $params) {
+
+	// extracts all key=>value pairs of the associative array as variables in the current scope 
+	extract($params);
+
+	require_once(__DIR__ . '/lib/MoodleClientConstants.php');
+	require_once(__DIR__ . '/config/config.php');
+	$GLOBALS['connection'] = $connection;
+	$GLOBALS['activeConnection'] = $activeConnection;
+	$moodle_events =& $moodle_events_reference();
+	require_once(__DIR__ . '/cis/get_events_by_userid.php');
+});
+
--- cis/get_events_by_userid.php
+++ cis/get_events_by_userid.php
@@ -1,13 +1,6 @@
 <?php
 
-require_once('../lib/MoodleAPI.php');
-require_once('../config/config.php');
-require_once(dirname(__DIR__).'/../../config/cis.config.inc.php');
-
-
-$username = $_GET['username'];
-$timestart = intval($_GET['timestart']);
-$timeend = intval($_GET['timeend']);
+require_once(dirname(__DIR__).'/lib/MoodleAPI.php');
 
 $moodleAPI = new MoodleAPI();
 header('Content-Type: application/json');
@@ -15,9 +8,11 @@ header('Content-Type: application/json');
 // Call the method
 $events = $moodleAPI->fhcomplete_events_by_userid($username,$timestart,$timeend);
 $tz = new DateTimeZone(date_default_timezone_get());
-foreach($events->events as $event){
+
+foreach($events as $event){
 	$timestart = new DateTime("@".$event->timestart);
 	$timestart->setTimezone($tz);
+	$timestart->modify('-1 seconds');
 	$event->timestart = $timestart->format(DateTime::ATOM);
 
 	$timeend = clone $timestart;
@@ -25,6 +20,6 @@ foreach($events->events as $event){
 	$event->timeend = $timeend->format(DateTime::ATOM);
 }
 
-echo json_encode($events); 
+$moodle_events = $events; 
 
 ?>
