c13bdc92f99a597466aa47987d5413bb764df2b2
Harald Bamberger <harald.bamberger@technikum-wien.at>
2025-05-14 17:44:26 +0200

    process events from moodle api only if an array is returned

--- cis/get_events_by_userid.php
+++ cis/get_events_by_userid.php
@@ -20,47 +20,49 @@ $timeend = $moodle_end_date->getTimestamp();
 $events = $moodleAPI->fhcomplete_events_by_userid($username, $timestart, $timeend, $langcode);
 $tz = new DateTimeZone(date_default_timezone_get());
 
-foreach($events as $event){
-	$tmptimestart = new DateTime("@".$event->timestart);
-	$tmptimestart->setTimezone($tz);
-	$tmptimestart->modify('-1 seconds');
-	$event->timestart = $tmptimestart->format(DateTime::ATOM);
+if(is_array($events))
+{
+	foreach($events as $event){
+		$tmptimestart = new DateTime("@".$event->timestart);
+		$tmptimestart->setTimezone($tz);
+		$tmptimestart->modify('-1 seconds');
+		$event->timestart = $tmptimestart->format(DateTime::ATOM);
 
-	$tmptimeend = clone $tmptimestart;
-	$tmptimeend->modify('+'.$event->timeduration.' seconds');
-	$event->timeend = $tmptimeend->format(DateTime::ATOM);
+		$tmptimeend = clone $tmptimestart;
+		$tmptimeend->modify('+'.$event->timeduration.' seconds');
+		$event->timeend = $tmptimeend->format(DateTime::ATOM);
 
-	$moodle_event_timestart = new DateTime($event->timestart);
-	$moodle_event_timeend = new DateTime($event->timeend);
+		$moodle_event_timestart = new DateTime($event->timestart);
+		$moodle_event_timeend = new DateTime($event->timeend);
 
-	$moodle_event = (object) array(
-		'type' => 'moodle',
-		'beginn' => $moodle_event_timestart->format('H:i:s'),
-		'ende' => $moodle_event_timeend->format('H:i:s'),
-		'allDayEvent' => true,
-		'datum' => $moodle_event_timestart->format('Y-n-j'),
-		'purpose' => $event->purpose,
-		'assignment' => $event->activityname,
-		'topic' => $event->activitystr,
-		'lektor' => [],
-		'gruppe' => [],
-		'ort_kurzbz' => $event->location,
-		'lehreinheit_id' => $event->lehreinheitsNummber ?? null,
-		'titel' => isset($event->course->fullname)? $event->course->fullname:null,
-		'lehrfach' => '',
-		'lehrform' => '',
-		'lehrfach_bez' => '',
-		'organisationseinheit' => '',
-		'farbe' => '00689E',
-		'lehrveranstaltung_id' => 0,
-		'ort_content_id' => 0,
-		'url' => $event->url,
-		'activityIcon' => isset($event->icon->iconurl)? $event->icon->iconurl:null,
-		'actionname' => isset($event->action->name)?$event->action->name:null,
-		'overdue' => !empty($event->overdue)
-	);
+		$moodle_event = (object) array(
+			'type' => 'moodle',
+			'beginn' => $moodle_event_timestart->format('H:i:s'),
+			'ende' => $moodle_event_timeend->format('H:i:s'),
+			'allDayEvent' => true,
+			'datum' => $moodle_event_timestart->format('Y-n-j'),
+			'purpose' => $event->purpose,
+			'assignment' => $event->activityname,
+			'topic' => $event->activitystr,
+			'lektor' => [],
+			'gruppe' => [],
+			'ort_kurzbz' => $event->location,
+			'lehreinheit_id' => $event->lehreinheitsNummber ?? null,
+			'titel' => isset($event->course->fullname)? $event->course->fullname:null,
+			'lehrfach' => '',
+			'lehrform' => '',
+			'lehrfach_bez' => '',
+			'organisationseinheit' => '',
+			'farbe' => '00689E',
+			'lehrveranstaltung_id' => 0,
+			'ort_content_id' => 0,
+			'url' => $event->url,
+			'activityIcon' => isset($event->icon->iconurl)? $event->icon->iconurl:null,
+			'actionname' => isset($event->action->name)?$event->action->name:null,
+			'overdue' => !empty($event->overdue)
+		);
 
-	$moodle_events[] = $moodle_event;
+		$moodle_events[] = $moodle_event;
+	}
 }
-
 ?>
